438f38968f8f771482b82ee8e851739c924ef329
kartikkrishnasharmaa <kartikkrishnasharmaa@gmail.com>
2025-07-20 11:20:54 +0530

    yuppp

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import SimpleLayout from "@/components/SimpleLayout";
 import Hero from "@/components/Hero";
 import Feature from "@/components/Feature";
+import Mcq from "@/components/Mcq";
 
 export default function Home() {
   return (
     <SimpleLayout>
       <Hero/>
       <Feature/>
+      <Mcq/>
     </SimpleLayout>
   );
 }
